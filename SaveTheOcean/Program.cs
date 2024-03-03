175aa21a45ee84eaaf5b80221bd056fc5aec55ec
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-03-03 04:36:04 +0100

    Remplaced magic strings

--- SaveTheOcean/Program.cs
+++ SaveTheOcean/Program.cs
@@ -7,6 +7,16 @@ namespace SaveTheOcean
     {
         static void Main(string[] args)
         {
+            const string StarterMenu = "Save the ocean!\r\nQuè vols fer?\r\n1. Jugar!\r\n2. Sortir";
+            const string JobMenu =
+                "Perfecte! Què vols ser?\r\n1. Tècnic (45 XP)\r\n2. Veterinari (80 XP)\r\n";
+            const string NameNemu = "Genial! I el teu nom?";
+            const string RescueMsg =
+                "Hola, {0}! El 112 ha rebut una trucada d’avís d’un exemplar a rescatar. \r\nLes dades que ens han donat són les següents:\r\n";
+            const string AnimalFileMsg = "Aquí tens la seva fitxa, per a més informació:\r\n";
+            const string MoveAnimal =
+                "El {0} té un grau d’afectació (GA) del {1}%. Vols curar-la aquí (1) o traslladar-la al centre (2)?";
+            const string GoodByeMsg = "Fins el proper rescat!\r\n";
             string user = "";
             string userJob;
             bool transfer;
@@ -14,7 +24,7 @@ namespace SaveTheOcean
 
             do
             {
-                Console.WriteLine("Save the ocean!\r\nQuè vols fer?\r\n1. Jugar!\r\n2. Sortir");
+                Console.WriteLine(StarterMenu);
                 user = Console.ReadLine() ?? "";
             } while (user != "1" && user != "2");
 
@@ -23,44 +33,39 @@ namespace SaveTheOcean
                 AAnimal animal = RescueFile.Specimen();
                 do
                 {
-                    Console.WriteLine(
-                        "Perfecte! Què vols ser?\r\n1. Tècnic (45 XP)\r\n2. Veterinari (80 XP)\r\n"
-                    );
+                    Console.WriteLine(JobMenu);
                     user = Console.ReadLine() ?? "";
                 } while (user != "1" && user != "2");
-
                 userJob = user == "1" ? "Tècnic" : "Veterinari";
-                Console.WriteLine("Genial! I el teu nom?");
 
-                Employee employee = new Employee(Console.ReadLine(), userJob);
+                Console.WriteLine(NameNemu);
+                Employee employee = new Employee(Console.ReadLine()??" ", userJob);
 
-                Console.WriteLine(
-                    "Hola, {0}! El 112 ha rebut una trucada d’avís d’un exemplar a rescatar. \r\nLes dades que ens han donat són les següents:\r\n",
-                    employee.Name
-                );
+                Console.WriteLine(RescueMsg, employee.Name);
 
                 RescueFile rescueFile = new RescueFile(animal, employee);
                 Console.WriteLine(rescueFile.ToString());
-                Console.WriteLine("Aquí tens la seva fitxa, per a més informació:\r\n");
+
+                Console.WriteLine(AnimalFileMsg);
                 Console.WriteLine(rescueFile.AAnimal.ToString());
 
                 do
                 {
-                    Console.WriteLine(
-                        "El {0} té un grau d’afectació (GA) del {1}%. Vols curar-la aquí (1) o traslladar-la al centre (2)?",
-                        rescueFile.AAnimal.Name,
-                        rescueFile.GA
-                    );
+                    Console.WriteLine(MoveAnimal, rescueFile.AAnimal.Name, rescueFile.GA);
                     user = Console.ReadLine() ?? "";
                 } while (user != "1" && user != "2");
 
                 transfer = user == "2";
-
                 Console.WriteLine(rescueFile.RescueDetails(transfer));
+
                 Console.WriteLine(employee.ToString());
-                Console.WriteLine("Fins el proper rescat!\r\n");
-                Console.WriteLine(animal.GetType());
-                Console.WriteLine(typeof(AAnimal));
+
+                Console.WriteLine(GoodByeMsg);
+            }
+            else
+            {
+                Console.WriteLine(GoodByeMsg);
+
             }
         }
     }
